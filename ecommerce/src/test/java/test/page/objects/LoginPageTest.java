f9b96c61db1a9ecfba2120bcc649dcf178ff7db1
Md Jahidul Islam <jahidul@Mds-MacBook-Pro.local>
2018-12-26 22:37:56 -0500

    google Api

--- ecommerce/src/test/java/test/page/objects/LoginPageTest.java
+++ ecommerce/src/test/java/test/page/objects/LoginPageTest.java
@@ -9,9 +9,12 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import page.objects.HomePage;
 import page.objects.LoginPage;
+import sheet.google.api.GoogleSheetReader;
 
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class LoginPageTest extends ApplicationPageBase {
 
@@ -26,7 +29,7 @@ public class LoginPageTest extends ApplicationPageBase {
         objHomePage = PageFactory.initElements(driver, HomePage.class);
     }
 
-    @DataProvider(name="DP")
+   /* @DataProvider(name="DP")
     public Object[][] getTestData() throws Exception{
         File filepath = new File(System.getProperty("user.dir") +  "/testData/TestData.xlsx");
         MyDataReader dr = new MyDataReader();
@@ -38,14 +41,6 @@ public class LoginPageTest extends ApplicationPageBase {
     }
 
 
-    /*@Test(dataProvider="DP")
-    public void invalidSignIn(String email, String password/*, String VerificationText*//*){
-
-        objHomePage.getLogInPage();
-        objLoginPage.login(email, password);
-
-    }*/
-
     @Test(dataProvider = "DP")
     public  void invalidSignin(String email, String password, String expectedErroMessage){
 
@@ -54,6 +49,30 @@ public class LoginPageTest extends ApplicationPageBase {
         String expectedText = expectedErroMessage;
         String actulText = objLoginPage.getErroMessage();
         Assert.assertEquals(actulText, expectedText);
+
+    }*/
+    @Test
+    public  void invalidSigninByGoogleSheetApi() throws IOException {
+
+       String spreadsheetId = "1AEVTjDK-RVcr0-DlAkTlW1HlsyqTOiCA1vS3Adrh230";
+       String range = "Sheet4!A2:D";
+       List<List<Object>> getRecords = GoogleSheetReader.getSpreadSheetRecords(spreadsheetId,range);
+
+        for (List cell: getRecords) {
+
+            objHomePage.getLogInPage();
+
+            objLoginPage.login(cell.get(1).toString(), cell.get(2).toString());
+
+            String expectedText = cell.get(3).toString();
+
+            String actulText = objLoginPage.getErroMessage();
+            Assert.assertEquals(actulText, expectedText);
+
+        }
+
+
+
 
     }
 }

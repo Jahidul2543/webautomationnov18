79b9e9296033bc80d023acc46d3e9a392fb40f5b
Md Jahidul Islam <jahidul@Mds-MacBook-Pro.local>
2018-12-28 16:08:57 -0500

    added UsingGoogleSheetLoginPageTest.java to read data from google sheet and use it by @DataProvider

--- ecommerce/src/test/java/test/page/objects/LoginPageTest.java
+++ ecommerce/src/test/java/test/page/objects/LoginPageTest.java
@@ -1,7 +1,7 @@
 package test.page.objects;
 
 import application.page.base.ApplicationPageBase;
-import com.xlsx.api.MyDataReader;
+import com.util.xlsx.reader.MyDataReader;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -29,23 +29,21 @@ public class LoginPageTest extends ApplicationPageBase {
         objLoginPage= PageFactory.initElements(driver, LoginPage.class);
         objHomePage = PageFactory.initElements(driver, HomePage.class);
     }
-    /*
+    /**
     *
     * Read Data from XLSX file
     *
     * */
-   /* @DataProvider(name="DP")
+
+    @DataProvider(name="DP")
     public Object[][] getTestData() throws Exception{
         File filepath = new File(System.getProperty("user.dir") +  "/testData/TestData.xlsx");
         MyDataReader dr = new MyDataReader();
         //Show me where is data file
         dr.setExcelFile(filepath.getAbsolutePath());
-
         String[][] data = dr.getExcelSheetData("Sheet3");
         return data;
     }
-
-
 
     @Test(dataProvider = "DP")
     public  void invalidSignin(String email, String password, String expectedErroMessage){
@@ -56,37 +54,5 @@ public class LoginPageTest extends ApplicationPageBase {
         String actulText = objLoginPage.getErroMessage();
         Assert.assertEquals(actulText, expectedText);
 
-    }*/
-    /*
-    *
-    * Read data from Google Sheet
-    * Make sure you have created client_secret.json from your account where you have Google Sheet Data file
-    * Delete .credentials directory from your module if it is exists. Other wise it will take existing/old credentials
-    * thus it may give unauthorized access error(error code starting with 4, such as 403,404)
-    *
-    * */
-    @Test
-    public  void invalidSigninByGoogleSheetApi() throws IOException {
-
-        Properties properties = loadProperties();
-        String spreadsheetId = properties.getProperty("GOOGLE.spreadsheetId");
-        String range = properties.getProperty("GOOGLE.range");
-        TestLogger.log("Using Sheet Number " + range.charAt(5)+ " and fields range  " + range.substring(6));
-        List<List<Object>> getRecords = GoogleSheetReader.getSpreadSheetRecords(spreadsheetId,range);
-
-            objHomePage.getLogInPage();
-
-        for (List cell: getRecords) {
-
-            objLoginPage.login(cell.get(0).toString(), cell.get(1).toString());
-
-            String expectedText = cell.get(2).toString();
-
-            String actulText = objLoginPage.getErroMessage();
-            Assert.assertEquals(actulText, expectedText);
-
-        }
-
     }
-
 }
